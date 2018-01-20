907f6f01a3951b4346376a156f193985cbc2a047
hezll <hezll@msn.com>
2018-01-20 17:37:56 +1100

    update test api

--- test/WechatApiTest.php
+++ test/WechatApiTest.php
@@ -6,31 +6,31 @@
  * Time: 15:48
  */
 include dirname(__DIR__) . "/autoload.php";
-$wechatApi = new \Wechat\WechatApi('wx80c4bf4cf0dae125', 'e01207cd547f62d73f5099aae83a9f15', 'e01207cd547f62d73f5099aae83a9f15');
+$wechatApi = new \Wechat\WechatApi('wx90c4bf2cf0dae125', 'e01207cd557f62c73f5099aae83a9f15', 'e01207cd557f62c73f5099aae83a9f15');
 $result = $wechatApi->sendTemplateMessage('{
-  "touser": "ov6Apv5hsUK7IACd14QpbjDOBtU8",
-  "template_id": "gwh2warlCMvXAyw4Pcxg6V7v22CLgnBx9de26DwxJa0",
-  "url": "http://weixin.qq.com/download",
+  "touser": "oIzphw8oUKZmr7q3v8H94wl9e92w",
+  "template_id": "9lPW5ofAo8QGxsp6gCfMe0ZBNbzc9zoezKhFrh9FhEA",
+  "url": "http://www.haoshupeini.com/module/novel/read.php?tid=3&nid=264&cid=2642c622b58f24c870ecc2b2169c08c6713&recent=1&checkin=1",
   "data": {
     "first": {
-      "value": "哈喽，恭喜你购买成功！${ceshi}",
-      "color": "#173177"
+      "value": "亲，今天你还没有签到哦~\n\n",
+      "color": "#0000CD"
     },
     "keyword1": {
-      "value": "龙虾",
-      "color": "#173177"
+      "value": "\n签到领取书币，回复\"退订签到通知\"将不再接收此消息，但你可能会错过几亿免费书币",
+      "color": ""
     },
-    "keyword2": {
-      "value": "39.8元",
-      "color": "#173177"
+   "keyword2": {
+      "value": "进行中\n\n",
+      "color": ""
     },
     "keyword3": {
-      "value": "2014年9月22日",
-      "color": "#173177"
+      "value": "今晚24:00前，别忘了哦\n\n",
+      "color": "#FF0000"
     },
     "remark": {
-      "value": "欢迎再次购买！",
-      "color": "#173177"
+      "value": "点击下方【抢书币】\n太好看啦每天送你免费书币⤵️⤵️ ",
+      "color": "#BC8F8F"
     }
   }
 }');
